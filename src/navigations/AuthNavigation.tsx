d8dc7a59d8b421d49c76f503997351bafc794c06
lyj92 <qlsek922@naver.com>
2026-02-03 23:23:23 +0900

    feat / 네이게이터 이동 및 stack 안에 스크린 옵션 객체화에 대한 코드추가

--- src/navigations/AuthNavigation.tsx
+++ src/navigations/AuthNavigation.tsx
@@ -5,10 +5,45 @@ import SignupScreen from "../screens/auth/SignupScreen";
 import { createStaticNavigation } from "@react-navigation/native";
 
 const AuthStack = createStackNavigator({
+  // 헤더 옵션들
+  screenOptions: {
+    headerTitleAlign: "center",
+    headerBackButtonDisplayMode: "minimal",
+    headerTintColor: "black",
+    // 헤더 스타일
+    headerStyle: {
+      backgroundColor: "white",
+      shadowColor: "gray",
+    },
+    // 헤더 폰트 스타일
+    headerTitleStyle: {
+      fontSize: 16,
+    },
+    // 공통 컨텐츠 영역 스타일
+    cardStyle: {
+      backgroundColor: "white",
+    },
+  },
   screens: {
-    AuthHome: AuthHomeScreen,
-    Login: LoginScreen,
-    Signup: SignupScreen,
+    // 스크린을 객체화 하고 그 안에 해당 스타일링 옵션을 추가할 수 있음
+    AuthHome: {
+      screen: AuthHomeScreen,
+      options: {
+        headerShown: false,
+      },
+    },
+    Login: {
+      screen: LoginScreen,
+      options: {
+        title: "로그인",
+      },
+    },
+    Signup: {
+      screen: SignupScreen,
+      options: {
+        title: "회원가입",
+      },
+    },
   },
 });
 
--- src/screens/auth/AuthHomeScreen.tsx
+++ src/screens/auth/AuthHomeScreen.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect } from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 interface AuthHomeScreenProps {}
@@ -8,9 +9,15 @@ interface AuthHomeScreenProps {}
  * @returns
  */
 function AuthHomeScreen({}: AuthHomeScreenProps) {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    console.log("hello world");
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text>AuthHomeScreen</Text>
+      <Text onPress={() => navigation.navigate("Login")}>로그인으로 이동</Text>
     </SafeAreaView>
   );
 }
