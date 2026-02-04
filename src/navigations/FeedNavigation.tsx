65ca4ea7798d5db8577d0f03c96bd6ecc3b869bf
lyj92 <qlsek922@naver.com>
2026-02-04 16:47:35 +0900

    feat / 피드 스크린, 피드 스택 설정

---
+++ src/navigations/FeedNavigation.tsx
@@ -0,0 +1,41 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import FeedListScreen from "../screens/feed/FeedListScreen";
+import FeedDetailScreen from "../screens/feed/FeedDetailScreen";
+import FeedFavoriteScreen from "../screens/feed/FeedFavoriteScreen";
+import EditLocationScreen from "../screens/feed/EditLocationScreen";
+import { colors } from "../constants/colors";
+
+export const FeedStack = createStackNavigator({
+  screenOptions: {
+    headerTitleAlign: "center",
+    headerBackButtonDisplayMode: "minimal",
+    headerTintColor: colors?.BLACK,
+    // 헤더 스타일
+    headerStyle: {
+      backgroundColor: colors?.WHITE,
+      shadowColor: colors?.GRAY_500,
+    },
+    // 헤더 폰트 스타일
+    headerTitleStyle: {
+      fontSize: 16,
+    },
+  },
+  screens: {
+    FeedList: {
+      screen: FeedListScreen,
+      options: {},
+    },
+    FeedDetail: {
+      screen: FeedDetailScreen,
+      options: {},
+    },
+    FeedFavortie: {
+      screen: FeedFavoriteScreen,
+      options: {},
+    },
+    Editlocation: {
+      screen: EditLocationScreen,
+      options: {},
+    },
+  },
+});
---
+++ src/screens/feed/EditLocationScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+interface EditLocationScreenProps {}
+
+function EditLocationScreen({}: EditLocationScreenProps) {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({});
+
+export default EditLocationScreen;
