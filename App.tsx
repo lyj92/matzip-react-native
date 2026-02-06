b7bba71daaf2b1f79c260f27ede4b3ebbdf358eb
lyj92 <qlsek922@naver.com>
2026-02-06 20:58:31 +0900

    feat / app.tsx queryProvider 설정

--- App.tsx
+++ App.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { StatusBar } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import RootNavigation from "./src/navigations/RootNavigation";
-
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "@/api/queryClient";
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent={true}
-      />
-      <RootNavigation />
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent={true}
+        />
+        <RootNavigation />
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
