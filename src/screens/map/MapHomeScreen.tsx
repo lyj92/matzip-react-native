4ec45fe54f4467ff9216206ea7047ee7f0bbb6ca
lyj92 <qlsek922@naver.com>
2026-02-09 01:16:19 +0900

    feat / 지도 홈 구글 맵 컴포넌트 연동 => 안드로이드 지도 안나오는 이슈

--- src/screens/map/MapHomeScreen.tsx
+++ src/screens/map/MapHomeScreen.tsx
@@ -3,20 +3,19 @@ import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import DrawerButton from "@/components/DrawerButton";
 import useAuth from "@/hooks/queries/useAuth";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 interface MapHomeScreenProps {}
 
 function MapHomeScreen({}: MapHomeScreenProps) {
   const { logoutMutation } = useAuth();
-  return (
-    <SafeAreaView>
-      <Text>MapHomeScreen</Text>
-      <DrawerButton />
-      <Text onPress={() => logoutMutation?.mutate(null)}>로그아웃</Text>
-    </SafeAreaView>
-  );
+  return <MapView style={styles?.containter} provider={PROVIDER_GOOGLE} />;
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  containter: {
+    flex: 1,
+  },
+});
 
 export default MapHomeScreen;
