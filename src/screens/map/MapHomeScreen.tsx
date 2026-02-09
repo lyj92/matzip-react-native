4ab700c003e070a5d5786118b3b36f703b05bd9c
lyj92 <qlsek922@naver.com>
2026-02-09 16:21:14 +0900

    feat / 현재 사용자 위도 경도 위치 설정

--- src/screens/map/MapHomeScreen.tsx
+++ src/screens/map/MapHomeScreen.tsx
@@ -1,20 +1,73 @@
-import React from "react";
-import { StyleSheet, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
 import DrawerButton from "@/components/DrawerButton";
 import useAuth from "@/hooks/queries/useAuth";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-
+import MapView, { LatLng, PROVIDER_GOOGLE } from "react-native-maps";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { colors } from "@/constants/colors";
+import Geolocation from "@react-native-community/geolocation";
 interface MapHomeScreenProps {}
 
 function MapHomeScreen({}: MapHomeScreenProps) {
   const { logoutMutation } = useAuth();
-  return <MapView style={styles?.containter} provider={PROVIDER_GOOGLE} />;
+
+  const [userLocation, setUserLocation] = useState<LatLng>();
+  const [isUserLocationError, setIsUserLocationError] =
+    useState<boolean>(false);
+
+  // inset 값 구하는 함수
+  const inset = useSafeAreaInsets();
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      (info) => {
+        setUserLocation(info.coords);
+      },
+      () => {
+        setIsUserLocationError(true);
+      },
+      {
+        enableHighAccuracy: true,
+      }
+    );
+  });
+
+  return (
+    <>
+      <DrawerButton
+        style={[styles?.drawerButton, { top: inset.top + 10 }]}
+        color={colors.WHITE}
+      />
+      <MapView
+        style={styles?.containter}
+        provider={PROVIDER_GOOGLE}
+        // 초기 경도 위도 설정
+        // initialRegion={{
+        //   latitude: info.coords.,
+        //   longitude: 126.978,
+        //   latitudeDelta: 0.0922,
+        //   longitudeDelta: 0.0421,
+        // }}
+      />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   containter: {
     flex: 1,
+  },
+  drawerButton: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    zIndex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    backgroundColor: colors.PINK_700,
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 50,
+    boxShadow: "1px 1px 3px rgba(0,0,0, 0.5)",
   },
 });
 
