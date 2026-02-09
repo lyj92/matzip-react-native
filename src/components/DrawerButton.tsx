ac39ea1d8e0fc27ee8c3e5cc6f68911bbe0cd174
lyj92 <qlsek922@naver.com>
2026-02-09 16:20:59 +0900

    feat / geolocation 라이브러리 설정 및 android, ios 추가 설정

--- src/components/DrawerButton.tsx
+++ src/components/DrawerButton.tsx
@@ -1,19 +1,34 @@
 import { DrawerNavigationProp } from "@react-navigation/drawer";
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Pressable, StyleSheet, View, Text } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  View,
+  Text,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
 import { MainDrawerParamList } from "@/types/navigation";
 import Ionicons from "@react-native-vector-icons/ionicons";
 import { colors } from "@/constants/colors";
 
 type Navigation = DrawerNavigationProp<MainDrawerParamList>;
 
-function DrawerButton() {
+interface DrawrButtonProps {
+  color?: string;
+  style?: StyleProp<ViewStyle>;
+}
+
+function DrawerButton({ style, color = colors?.BLACK }: DrawrButtonProps) {
   const navigation = useNavigation<Navigation>();
   return (
-    <Pressable style={styles.container} onPress={() => navigation.openDrawer()}>
+    <Pressable
+      style={[styles.container, style]}
+      onPress={() => navigation.openDrawer()}
+    >
       {/* <Text style={styles?.button}>서랍</Text> */}
-      <Ionicons name="menu" size={25} color={colors?.BLACK} />
+      <Ionicons name="menu" size={25} color={color} />
     </Pressable>
   );
 }
