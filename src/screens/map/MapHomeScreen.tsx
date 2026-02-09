9a991f9646e50876bd3c64c8f54d32851f588b3c
lyj92 <qlsek922@naver.com>
2026-02-09 17:40:41 +0900

    feat / 구글 지도 api 연동

--- src/screens/map/MapHomeScreen.tsx
+++ src/screens/map/MapHomeScreen.tsx
@@ -1,19 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { Pressable, StyleSheet, View } from "react-native";
 import DrawerButton from "@/components/DrawerButton";
 import useAuth from "@/hooks/queries/useAuth";
 import MapView, { LatLng, PROVIDER_GOOGLE } from "react-native-maps";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { colors } from "@/constants/colors";
+import FontAwesome6 from "@react-native-vector-icons/fontawesome6";
 import Geolocation from "@react-native-community/geolocation";
 interface MapHomeScreenProps {}
 
 function MapHomeScreen({}: MapHomeScreenProps) {
   const { logoutMutation } = useAuth();
 
-  const [userLocation, setUserLocation] = useState<LatLng>();
+  const [userLocation, setUserLocation] = useState<LatLng>({
+    latitude: 37.5516032365118,
+    longitude: 126.98989626020192,
+  });
   const [isUserLocationError, setIsUserLocationError] =
     useState<boolean>(false);
+
+  const mapRef = useRef<MapView | null>(null);
+
+  const moveMapView = (coordinate: LatLng) => {
+    mapRef.current?.animateToRegion({
+      ...coordinate,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  };
+
+  const handlePressUserLocation = () => {
+    if (isUserLocationError) {
+      return;
+    }
+    moveMapView(userLocation);
+  };
 
   // inset 값 구하는 함수
   const inset = useSafeAreaInsets();
@@ -39,16 +60,26 @@ function MapHomeScreen({}: MapHomeScreenProps) {
         color={colors.WHITE}
       />
       <MapView
+        ref={mapRef}
         style={styles?.containter}
         provider={PROVIDER_GOOGLE}
-        // 초기 경도 위도 설정
-        // initialRegion={{
-        //   latitude: info.coords.,
-        //   longitude: 126.978,
-        //   latitudeDelta: 0.0922,
-        //   longitudeDelta: 0.0421,
-        // }}
+        region={{
+          ...userLocation,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
       />
+
+      <View style={styles?.buttonList}>
+        <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+          <FontAwesome6
+            name="location-crosshairs"
+            iconStyle="solid"
+            size={25}
+            color={colors.WHITE}
+          />
+        </Pressable>
+      </View>
     </>
   );
 }
@@ -68,6 +99,22 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 50,
     borderBottomRightRadius: 50,
     boxShadow: "1px 1px 3px rgba(0,0,0, 0.5)",
+  },
+  mapButton: {
+    backgroundColor: colors.PINK_700,
+    marginVertical: 5,
+    height: 45,
+    width: 45,
+    borderRadius: 45,
+    alignItems: "center",
+    justifyContent: "center",
+    boxShadow: "1px 1px 3px rgba(0,0,0, 0.5)",
+  },
+  buttonList: {
+    position: "absolute",
+    bottom: 30,
+    right: 20,
+    zIndex: 1,
   },
 });
 
